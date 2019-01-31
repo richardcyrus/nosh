ad258a11dc7ace0a4b82f753d691d4e169ada57f
tjohn130 <tjohn130@email.cpcc.edu>
2019-01-31 01:42:56 -0500

    Icon Click Event

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -44,37 +44,56 @@ $("#slider01").roundSlider({
 });
 
 //icon selector//
-$(".imgBtn").click(function(e){
+$(".iconDiv").click(function(e){
     e.preventDefault();
+
+    $(this).find('i').removeClass("pe-is-f-sushi-2");
+
+    foodList = {
+        American:"pe-is-f-hot-dog-f",
+        Asian:"pe-is-f-sushi-4-f",
+        BBQ:"pe-is-f-barbecue-2-f",
+        Bakery:"pe-is-f-bread-3-f",
+        Bar_Food:"pe-is-f-cork-skrew-f",
+        Beverage:"pe-is-f-beer-bottle",
+        Breakfest:"pe-is-f-waffle-f",
+        Burger:"pe-is-f-burger-1",
+        Cafe:"pe-is-f-coffee-maker-2-f",
+        Chinese:"pe-is-f-chinese-food-53",
+        Coffee_and_Tea:"pe-is-f-cup-2",
+        Dessert:"pe-is-f-piece-of-cake-1",
+        Donuts:"pe-is-f-donut-2-f",
+        Drinks_Only:"pe-is-f-wine-glass-3-f",
+        Fast_Food:"pe-is-f-french-fries-f",
+        French:"pe-is-f-croissant",
+        German:"pe-is-f-sausage-1-f",
+        Hawaiian:"pe-is-f-anana",
+        Healthy_Food:"pe-is-f-apple-2-f",
+        Ice_Cream:"pe-is-f-ice-cream-2",
+        Italian:"pe-is-f-chef-hat-f",
+        Japanese:"pe-is-f-sushi-2",
+        Juices:"pe-is-f-pitcher-2",
+        Kebab:"pe-is-f-skewer",
+        Mediterranean:"m",
+        Mexican:"pe-is-f-kebab",
+        Pizza:"pe-is-f-pizza-2",
+        Polish:"pe-is-f-sausage-1",
+        Pub_Food:"pe-is-f-beer-glass-f",
+        Salad:"pe-is-f-wheat-f",
+        Sandwich:"pe-is-f-sandwich",
+        Seafood:"pe-is-f-fish-1",
+        Steak:"pe-is-f-steak-2",
+        Sushi:"pe-is-f-sushi-2-f",
+        Taco:"pe-is-f-kebab-f",
+        Tea:"pe-is-f-kettle-1"
+    }
+
+    var foodArray = Object.keys(foodList)
+
+    counter++;
     
-    var images = [{imgUrl:"https://image.flaticon.com/icons/svg/1046/1046784.svg",imgDes:"Burger",imgNum:0},{imgUrl:"https://image.flaticon.com/icons/svg/135/135763.svg",imgDes:"Fries",imgNum:1},{imgUrl:"https://image.flaticon.com/icons/svg/706/706164.svg",imgDes:"Boot",imgNum:2}]
+    $(this).find('i').addClass(foodList.Stringify(foodArray[counter]));
 
-    switch($(this).attr("data-typeBtn")){
-        case "forward":
-        counter++;
-        break;
-        case "back":
-        counter--;
-        break;
-    }
-
-    if(counter >= images.length-1){
-        $("#food").attr("src", images[counter].imgUrl);
-        $("#foodValue").text(images[counter].imgDes);
-        counter = 0;
-    }
-    else if(counter < 0){
-        counter = images.length-1;
-        $("#food").attr("src", images[counter].imgUrl);
-        $("#foodValue").text(images[counter].imgDes);
-    }
-    else{
-        $("#food").attr("src", images[counter].imgUrl);
-        $("#foodValue").text(images[counter].imgDes);
-        counter++;
-        
-    }
-
-    
+    // $(this).find('i').removeClass(foodList.American).addClass(foodList.BBQ);
 })
 });
