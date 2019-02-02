aa11f0544fda71633bbd0493b07b2f65a21af1aa
tjohn130 <tjohn130@email.cpcc.edu>
2019-02-01 21:13:29 -0500

    fixed buttons for changing icons on main.js

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,98 +1,241 @@
 //fade away for the nosh title//
-  $(window).scroll(function(){
-    $(".noshSvg").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
+ $(window).scroll(function(){
+   $(".noshSvg").css("opacity", 1 - $(window).scrollTop() / 250);
+ });
 
-  //fade away for the start now button//
-  $(window).scroll(function(){
-    $(".startBtn").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
+ //fade away for the start now button//
+ $(window).scroll(function(){
+   $(".startBtn").css("opacity", 1 - $(window).scrollTop() / 250);
+ });
 
-  //auto scroll after clicking the about button//
-  $(".aboutArrow").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".aboutDiv").offset().top},
-        2000);
+ //auto scroll after clicking the about button//
+ $(".aboutArrow").click(function() {
+   $('html,body').animate({
+       scrollTop: $(".aboutDiv").offset().top},
+       2000);
 });
 
 //distance selector//
 $(document).ready(function(){
-    
-    var counter = 0;
+   foodList = [
+       {
+        className:"pe-is-f-hot-dog-f",
+        category: "American" 
+    },
+        {
+        className:"pe-is-f-sushi-4-f",
+        category:"Asian"
+    },
+        {
+       className:"pe-is-f-barbecue-2-f",
+       category: "BBQ"
+    },
+        {
+       className:"pe-is-f-bread-3-f", 
+       category: "Bakery"
+    },
+        {
+       className:"pe-is-f-cork-skrew-f", 
+       category: "Bar Food"
+    },
+        {
+       className:"pe-is-f-beer-bottle", 
+       category: "Beverage"
+    },
+       {
+       className:"pe-is-f-waffle-f",
+       category: "Breakfest"
+    },
+    {
+       className:"pe-is-f-burger-1", 
+       category: "Burger"
+    },
+       {
+       className:"pe-is-f-coffee-maker-2-f", 
+       category: "Cafe"
+    },
+        {
+       className:"pe-is-f-chinese-food-53", 
+       category: "Chinese"
+    },
+        {
+       className:"pe-is-f-cup-2", 
+       category: "Coffee and Tea"
+    },
+        {
+       className:"pe-is-f-piece-of-cake-1", 
+       category: "Dessert"
+    },
+        {
+       className:"pe-is-f-donut-2-f", 
+       category: "Donuts"
+    },
+        {
+       className:"pe-is-f-wine-glass-3-f", 
+       category: "Drinks Only"
+    },
+        {
+       className:"pe-is-f-french-fries-f", 
+       category: "Fast Food"
+    },
+        {
+       className:"pe-is-f-croissant", 
+       category: "French"
+    },
+        {
+       className:"pe-is-f-sausage-1-f", 
+       category: "German"
+    },
+        {
+       className:"pe-is-f-anana", 
+       category: "Hawaiian"
+    },
+        {
+       className:"pe-is-f-apple-2-f", 
+       category: "Healthy Food"
+    },
+        {
+       className:"pe-is-f-ice-cream-2", 
+       category: "Ice Cream"
+    },
+        {
+       className:"pe-is-f-chef-hat-f", 
+       category: "Italian"
+    },
+        {
+       className:"pe-is-f-sushi-2", 
+       category: "Japanese"
+    },
+        {
+       className:"pe-is-f-pitcher-2", 
+       category: "Juices"
+    },
+        {
+       className:"pe-is-f-skewer", 
+       category: "Kebab"
+    },
+        {
+       className:"m", 
+       category: "Mediterranean"
+    },
+        {
+       className:"pe-is-f-kebab", 
+       category: "Mexican"
+    },
+        {
+       className:"pe-is-f-pizza-2", 
+       category: "Pizza"
+    },
+        {
+       className:"pe-is-f-sausage-1", 
+       category: "Polish"
+    },
+       {
+       className:"pe-is-f-beer-glass-f", 
+       category: "Pub Food"
+    },
+       {
+       className:"pe-is-f-wheat-f", 
+       category: "Salad"
+    },
+       {
+       className:"pe-is-f-sandwich", 
+       category: "Sandwich"
+    },
+       {
+       className:"pe-is-f-fish-1", 
+       category: "Seafood"
+    },
+       {
+       className:"pe-is-f-steak-2", 
+       category: "Steak"
+    },
+       {
+       className:"pe-is-f-sushi-2-f", 
+       category: "Sushi"
+    },
+       {
+       className:"pe-is-f-kebab-f", 
+       category: "Taco"
+    },
+       {
+       className:"pe-is-f-kettle-1"
+   , category: "Tea"}]
+
+   
+   var counter = 0;
 
 $("#slider01").roundSlider({
-    min: 2,
-    max: 20,
-    step: 1,
-    value: 2,
-    radius: 170,
-    startAngle: 90,
-    handleSize: "+20",
-    sliderType: "min-range",
+   min: 2,
+   max: 20,
+   step: 1,
+   value: 2,
+   radius: 170,
+   startAngle: 90,
+   handleSize: "+20",
+   sliderType: "min-range",
 
-    showTooltip: false,
-    tooltipFormat: function (e) {
-        var handle = this._handles().html("<div class='inner-tooltip'></div>");
-        handle.children().html(this.options.value).rsRotate(-e.handle.angle);
-        return this.options.rangeValue;
-    },
-    change: function(e){
-        $("#sliderV1").html(e.value);
-    }
+   showTooltip: false,
+   tooltipFormat: function (e) {
+       var handle = this._handles().html("<div class='inner-tooltip'></div>");
+       handle.children().html(this.options.value).rsRotate(-e.handle.angle);
+       return this.options.rangeValue;
+   },
+   change: function(e){
+       $("#sliderV1").html(e.value);
+   }
 
 });
 
 //icon selector//
-$(".iconDiv").click(function(e){
-    e.preventDefault();
+$("#rightBtn").click(function(e){
+   e.preventDefault();
+   var iselector = $(".iconDiv").find('i');
+   iselector.removeClass("pe-is-f-sushi-2");
+   
+   if(counter > foodList.length){
 
-    $(this).find('i').removeClass("pe-is-f-sushi-2");
+        
+        counter = 0;
+        iselector.addClass(foodList[counter].className);
+        console.log("if"+counter)
+        
+   }
+   else{
+        iselector.removeClass(foodList[counter].className)
+        counter++;
+        iselector.addClass(foodList[counter].className);
+        console.log("else"+counter)
+        
+   }
+   
+   
 
-    foodList = { 
-        American:"pe-is-f-hot-dog-f",
-        Asian:"pe-is-f-sushi-4-f",
-        BBQ:"pe-is-f-barbecue-2-f",
-        Bakery:"pe-is-f-bread-3-f",
-        Bar_Food:"pe-is-f-cork-skrew-f",
-        Beverage:"pe-is-f-beer-bottle",
-        Breakfest:"pe-is-f-waffle-f",
-        Burger:"pe-is-f-burger-1",
-        Cafe:"pe-is-f-coffee-maker-2-f",
-        Chinese:"pe-is-f-chinese-food-53",
-        Coffee_and_Tea:"pe-is-f-cup-2",
-        Dessert:"pe-is-f-piece-of-cake-1",
-        Donuts:"pe-is-f-donut-2-f",
-        Drinks_Only:"pe-is-f-wine-glass-3-f",
-        Fast_Food:"pe-is-f-french-fries-f",
-        French:"pe-is-f-croissant",
-        German:"pe-is-f-sausage-1-f",
-        Hawaiian:"pe-is-f-anana",
-        Healthy_Food:"pe-is-f-apple-2-f",
-        Ice_Cream:"pe-is-f-ice-cream-2",
-        Italian:"pe-is-f-chef-hat-f",
-        Japanese:"pe-is-f-sushi-2",
-        Juices:"pe-is-f-pitcher-2",
-        Kebab:"pe-is-f-skewer",
-        Mediterranean:"m",
-        Mexican:"pe-is-f-kebab",
-        Pizza:"pe-is-f-pizza-2",
-        Polish:"pe-is-f-sausage-1",
-        Pub_Food:"pe-is-f-beer-glass-f",
-        Salad:"pe-is-f-wheat-f",
-        Sandwich:"pe-is-f-sandwich",
-        Seafood:"pe-is-f-fish-1",
-        Steak:"pe-is-f-steak-2",
-        Sushi:"pe-is-f-sushi-2-f",
-        Taco:"pe-is-f-kebab-f",
-        Tea:"pe-is-f-kettle-1"
-    }
+   
+   // $(this).find('i').removeClass(foodList.American).addClass(foodList.BBQ);
+})
 
-    var foodArray = Object.keys(foodList)
+$("#leftBtn").click(function(e){
+   e.preventDefault();
+   var iselector = $(".iconDiv").find('i');
 
-    counter++;
-    
-    $(this).find('i').addClass(foodList.BBQ);
+   iselector.removeClass("pe-is-f-sushi-2");
+   
+   if(counter < 0){
+        console.log("if"+ counter)
+       counter = foodList.length -1;
+       iselector.addClass(foodList[counter].className)
+       
+   }
+   else{
 
-    // $(this).find('i').removeClass(foodList.American).addClass(foodList.BBQ);
+        iselector.removeClass(foodList[counter].className)
+        counter--;
+        iselector.addClass(foodList[counter].className)
+        console.log("else"+counter)
+        
+   }
+   
+   // $(this).find('i').removeClass(foodList.American).addClass(foodList.BBQ);
 })
 });
